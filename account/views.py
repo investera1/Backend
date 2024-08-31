602dc81a73c463fb32a232db17b24879ddce8363
AbdullahKadad <abdullah.qdad.2@gmail.com>
2024-08-31 14:56:50 +0300

    abdullah

--- account/views.py
+++ account/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CustomTokenObtainPairSerializer, RegisterUserSerializer
 
-class HomeView():
-    pass
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
