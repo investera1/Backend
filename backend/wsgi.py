29d798defb6526a7ad40f616f009a9e472e025f3
AsaadAlmughrabi <assadalmograbe8@gmail.com>
2024-09-09 22:49:50 +0000

    deploy on vercel

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling  # For serving static files in production
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_wsgi_application()
+# Wrap the WSGI application with Cling to serve static files
+application = Cling(get_wsgi_application())
+app =application
